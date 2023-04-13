98461b7cb44e3cd48dae122b8864bc0972d0d883
kevinjapa <84851902+kevinjapa@users.noreply.github.com>
2023-04-13 08:29:33 -0500

    3 files modified

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
-function validar
-{
-  alert("Pronto estaremos en Contacto");
+console.log('UPS Ecuador');
+
+/*var i=0 ;
+
+var elemento = document.getElementById("titulo") //es un elemento de html
+elemento.addEventListener("click", function() {
+//alert("click en titulo")
+elemento.innerHTML=i
+});
+
+var i=0 ;
+ elemento.addEventListener("mouseover",function(){
+   i=i+1
+   console.log(i)
+   elemento.inert
+ })*/
+/*
+var menu = document.getEleme
+ var listas = document.getElementsByTagName("li") // hago rereferencia li , lista
+ 
+ console.log(listas)
+
+ var list = document.querySelector("#menu, li")
+ console.log(list);
+*/
+ var list2=document.querySelector("main li:nth-child(odd")
+ console.log(list2);
+
+ /* Como crear un elemento.  boton*/
+
+// Creamos un nuevo elemento de tipo p
+const nuevoElemento = document.createElement("p");
+// Agregamos contenido al elemento
+nuevoElemento.textContent = "¡Hola, soy un nuevo párrafo!";
+// Agregamos el elemento al body del documento
+document.body.appendChild(nuevoElemento);
+
+function AddLink(){
+  console.log("good");
 }
